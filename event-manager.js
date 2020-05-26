51016e1a729cf2b8dffcaa6d48923c8401a5c1ac
NicoSchoeman <9889916+nico-schoeman@users.noreply.github.com>
2020-05-26 11:28:47 +0200

    arguments and interceptors

--- event-manager.js
+++ event-manager.js
@@ -1,24 +1,35 @@
-export default function EventManager () {
-    this.listeners = [];
+export default function EventManager() {
+	this.listeners = [];
+	this.interceptors = [];
 }
 
-EventManager.prototype.AddListener = function (key, callback) {
-    if (!this.listeners.find(listener => listener.key === key))
-        this.listeners.push({key: key, callbacks: []});
-    let event = this.listeners.find(listener => listener.key === key);
-    event.callbacks.push(callback);
-}
+EventManager.prototype.AddInterceptor = function(key, handler) {
+	if (!this.interceptors.find(interceptor => interceptor.key === key)) this.interceptors.push({ key: key, handlers: [] });
+	let interceptor = this.interceptors.find(interceptor => interceptor.key === key);
+	interceptor.handlers.push(handler);
+};
 
-EventManager.prototype.RemoveListener = function (key, remove) {
-    let event = this.listeners.find(listener => listener.key === key);
-    if (event) {
-        event.callbacks = event.callbacks.filter(callback => callback != remove);
-    }
-}
+EventManager.prototype.AddListener = function(key, callback) {
+	if (!this.listeners.find(listener => listener.key === key)) this.listeners.push({ key: key, callbacks: [] });
+	let event = this.listeners.find(listener => listener.key === key);
+	event.callbacks.push(callback);
+};
 
-EventManager.prototype.TriggerEvent = function (key) {
-    let event = this.listeners.find(listener => listener.key === key);
-    if (event) {
-        event.callbacks.forEach(callback => callback());
-    }
-}
+EventManager.prototype.RemoveListener = function(key, remove) {
+	let event = this.listeners.find(listener => listener.key === key);
+	if (event) {
+		event.callbacks = event.callbacks.filter(callback => callback != remove);
+	}
+};
+
+EventManager.prototype.TriggerEvent = async function(key, ...args) {
+  let interceptor = this.interceptors.find(interceptor => interceptor.key === key);
+  if (interceptor) {
+    interceptor.handlers.forEach(handler => args = handler(...args));
+  }
+
+	let event = this.listeners.find(listener => listener.key === key);
+	if (event) {
+		event.callbacks.forEach(callback => callback(...args));
+	}
+};
